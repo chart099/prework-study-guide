f39089b1ab8672def5548f739d69b937ac5656c7
chart099 <122659980+chart099@users.noreply.github.com>
2023-09-10 21:46:47 -0500

    added for loop

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var shape = "circle";
 
 var topics = "HTML, CSS, Git, JavaScript";
-let topic = "Coding"
 console.log(topics);
-console.log(topic);
+
+
+var topic = "HTML";
+
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
